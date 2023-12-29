d579c25d0364d392efa3c3173bdc991325ec2800
Yura <tarachom@gmail.com>
2023-12-29 20:21:28 +0200

    up

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#Копіювання необхідних файлів із основного проекту перед збіркою
+#Копіювання необхідних файлів із основного проекту https://github.com/tarachom/FindOrgUaSite перед збіркою
+#Проект https://github.com/tarachom/FindOrgUaSite потрібно клонувати на локальну машину
 
 cp ../FindOrgUa/Configurator/CodeGeneration.cs            Configurator/
 cp ../FindOrgUa/ФункціїТаМодулі/Triggers.cs               Configurator/
